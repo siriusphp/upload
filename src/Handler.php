6ecca2e89a12ffc426cc62f8a07263fca023024e
Adrian Miu <adrian@adrianmiu.ro>
2014-01-09 22:49:09 +0200

    More progress on the upload handler

--- src/Handler.php
+++ src/Handler.php
@@ -5,71 +5,103 @@ use Sirius\Upload\Container\ContainerInterface;
 use Sirius\Upload\Container\Local as LocalContainer;
 use Sirius\Upload\Exception\InvalidContainerException;
 
-class Handler {
+class Handler implements UploadHandlerInterface {
     /**
      * @var ContainerInterface
      */
     protected $container;
     
-    protected $webLocation = '/';
-    
+    /**
+     * Prefix to be added to the file.
+     * It can be a subfolder or just a string to be used as prefix
+     * 
+     * @var string
+     */
     protected $prefix = '';
     
+    /**
+     * When uploading a file that has the same name as a file that is
+     * already in the container should it overwrite it or use another name
+     * 
+     * @var boolean
+     */
     protected $overwrite = false;
     
-    function __construct($directoryOrContainer, $webLocation = '/', $prefix = '', $overwrite = false) {
+    function __construct($directoryOrContainer, $prefix = '', $overwrite = false) {
         if ($directoryOrContainer instanceof ContainerInterface) {
-            $this->setContainer($directoryOrContainer);
+            $this->container = $directoryOrContainer;
         } elseif (is_string($directoryOrContainer)) {
-            $this->setContainer(new LocalContainer($directoryOrContainer));
+            $this->container = new LocalContainer($directoryOrContainer);
         }
         if (!$this->container) {
             throw new InvalidContainerException('Destination container for uploaded files is missing');
-        }
-        if (!$this->container->isWritable()) {
-            throw new InvalidContainerException('Destination container for uploaded files is not writable');
         }
         $this->webLocation = (string)$webLocation;
         $this->prefix = (string)$prefix;
         $this->overwrite = (bool)$overwrite;
     }
     
-    function setContainer(ContainerInterface $container) {
-        $this->container = $container;
-        return $this;
-    }
-    
-    function getContainer() {
-        return $this->container;
-    }
-    
     function process($files = array()) {
-        $files = $this->normalizeFiles($files);
+        $this->files = $this->normalizeFiles($files);
+        foreach ($this->files as $k => $file) {
+            $files[$k] = $this->processSingleFile($file);
+        }
+        return $this->files;
+    }
+    
+    function clear($file) {
+        $files = is_array($file) ? $file : array($file);
+        foreach ($files as $file) {
+            $this->container->delete($file);
+            $this->container->delete($file . '.lock');
+        }
+    }
+    
+    function confirm($file) {
+        $files = is_array($file) ? $file : array($file);
+        foreach ($files as $file) {
+            $this->container->delete($file . '.lock');
+        }
+    }
+    
+    protected function processSingleFile($file) {
+        
     }
     
     protected function normalizeFiles($files) {
+        // wrong format, go away
         if (!is_array($files) || !isset($files['name'])) {
             return array();
         }
+        $result = array();
+        // we have list of files, which PHP messes up
         if (is_array($files['name'])) {
-            $result = array();
             foreach ($files['name'] as $k => $v) {
                 $result[$k] = array(
                     'name' => $this->fixUploadedFileName($files['name'][$k]),
                     'type' => $files['type'][$k],
-                    'size' => $files['size'[$k],
+                    'size' => $files['size'][$k],
                     'error' => $files['error'][$k],
                     'tmp_name' => $files['tmp_name'][$k]
                 );
             }
             $files = $result;
-        } else {
+        // we have a single file
+        } elseif (isset($files['name'])) {
             $files['name'] = $this->fixUploadedFileName($files['name']);
+            $result = array($files);
+        // we have a list of files which are in correct format
+        } elseif (isset($files[0]) && isset($files[0]['name'])) {
+            foreach ($files as $k => $file) {
+                $files[$k]['name'] = $this->fixUploadedFileName($file['name']);
+            }
+            $result = $files;
         }
-        return $files;
+        return $result;
     }
     
     protected function fixUploadedFileName($name) {
-        return $name;
+        $name = preg_replace('/[^a-z0-9]+/', '_', strtolower($name));
+        return preg_replace('/[_]+/', '_', $name);
     }
 }
