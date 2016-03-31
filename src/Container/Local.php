33e4a61f46167c1a7c22021c7e590e78b16722ac
Adrian Miu <adrian@adrianmiu.ro>
2016-03-31 14:41:06 +0300

    Remove the usage of is_uploaded_file() and move_uploaded_file() because the don't work for AJAX-based uploads

--- src/Container/Local.php
+++ src/Container/Local.php
@@ -92,12 +92,18 @@ class Local implements ContainerInterface
     public function moveUploadedFile($localFile, $destination)
     {
         $dir = dirname($this->baseDirectory . $destination);
-        if ($this->ensureDirectory($dir) && file_exists($localFile)) {
-            if (is_uploaded_file($localFile)) {
-                return move_uploaded_file($localFile, $this->baseDirectory . $destination);
+        if (file_exists($localFile) && $this->ensureDirectory($dir)) {
+            /**
+             * we could use is_uploaded_file() and move_uploaded_file()
+             * but in case of ajax uploads that would fail
+             */
+            if (is_readable($localFile)) {
+                // rename() would be good but this is better because $localFile may become 'unwritable'
+                $result = copy($localFile, $this->baseDirectory . $destination);
+                @unlink($localFile);
+                return $result;
             }
         }
-
         return false;
     }
 
